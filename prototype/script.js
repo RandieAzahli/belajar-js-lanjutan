3b978923af97ba10530d7d77f23a55111de8480c
RandieAzahli <randyboled@outlook.com>
2022-07-12 00:10:24 +0700

    reload files

--- prototype/script.js
+++ prototype/script.js
@@ -1,6 +1,7 @@
 // ? membuat object dengan berbagai cara
 
 // * object literal
+// ! tidak efektif untuk object yang relatif banyak
 
 // let mahasiswa1 = {
 //   nama: "Randie",
@@ -21,6 +22,7 @@
 // };
 
 // * function declaration
+// ! Duplikasi data
 
 // function Mahasiswa(nama, energy) {
 //   let mahasiswa = {};
@@ -42,20 +44,48 @@
 // let Randi = Mahasiswa("Randi", 10);
 // let Sandi = Mahasiswa("Sandi", 20);
 
-// * constructor function
+// // * constructor function
+
+// function Mahasiswa(nama, energy) {
+//   this.nama = nama;
+//   this.energy = energy;
+//   this.makan = function (porsi) {
+//     this.energy += porsi;
+//     console.log(`Halo ${this.nama} ! selamat makan ...`);
+//   };
+
+//   this.main = function (jam) {
+//     this.energy -= jam;
+//     console.log(`Halo ${this.nama} ! selamat bermain ...`);
+//   };
+// }
+
+// let randi = new Mahasiswa("Randi", 20);
+
+// * object.create()
+
+let methodeMahasiswa = {
+    makan: function (porsi) {
+        this.energy += porsi;
+        console.log(`Hallo ${this.nama} ! selamat makan ...`);
+    },
+
+    main: function (jam) {
+        this.energy -= jam;
+        console.log(`Hallo ${this.nama} ! selamat bermain ...`);
+    },
+
+    tidur: function (jam) {
+        this.energy += jam * 2;
+        console.log(`Hallo ${this.nama} ! selamat tidur ...`);
+    },
+};
 
 function Mahasiswa(nama, energy) {
-  this.nama = nama;
-  this.energy = energy;
-  this.makan = function (porsi) {
-    this.energy += porsi;
-    console.log(`Halo ${this.nama} ! selamat makan ...`);
-  };
-
-  this.main = function (jam) {
-    this.energy -= jam;
-    console.log(`Halo ${this.nama} ! selamat bermain ...`);
-  };
+    let mahasiswa = Object.create(methodeMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energy = energy;
+    return mahasiswa;
 }
 
-let randi = new Mahasiswa("Randi", 20);
+let randi = Mahasiswa("Randi", 20);
